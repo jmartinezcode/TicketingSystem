c5a52ac36a198394fee639014bd6684e3fcc0555
jmartinezcode <jmartinez.data@gmail.com>
2023-10-09 16:09:33 -0500

    Created Ticket class

---
+++ Ticket.cs
@@ -0,0 +1,15 @@
+public class Ticket
+{
+    public int TicketID { get; set; }
+    public string Summary { get; set;  }
+    public string Status { get; set; }
+    public string Priority { get; set; }
+    public string Submitter { get; set; }
+    public string Assigned { get; set; }
+    public List<string> Watching { get; set; }
+    public override string ToString()
+    {
+        return $"TicketID: {TicketID}\nSummary: {Summary}\nStatus: {Status}\nPriority: {Priority}\nSubmitter: {Submitter}\nAssigned: {Assigned}\nWatching: {string.Join("|", Watching)}\n";
+    }
+
+}
