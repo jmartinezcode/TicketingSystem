cd092bc6937742c799c3a4b932ed57235dcc7631
jmartinezcode <jmartinez.data@gmail.com>
2023-10-09 17:36:59 -0500

    Updated to use ticketFile class to display

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using NLog;
+﻿using System.ComponentModel;
+using NLog;
 
 string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 
@@ -7,6 +8,8 @@ var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 
 string file = "Tickets.csv";
 logger.Info("Program started");
+
+TicketFile ticketFile = new TicketFile(file);
 
 string choice;
 // TicketID, Summary, Status, Priority, Submitter, Assigned, Watching
@@ -23,58 +26,35 @@ do
     // read tickets
     if (choice == "1")
     {
-        if (File.Exists(file))
-        {
-            StreamReader sr = new StreamReader(file);
-            while (!sr.EndOfStream)
-            {
-                string line = sr.ReadLine();
-                string[] arr = line.Split(",");
-                // string[] watching = arr[6].Split("|");
-                Console.WriteLine("\nTicketID: {0}\nSummary: {1}\nStatus: {2}\nPriority: {3}\nSubmitter: {4}\nAssigned: {5}\nWatching: {6}\n", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
-            }
-            sr.Close();
-        }
-        // else
-        // {
-        //     Console.WriteLine("File does not exist");
-        // }
+        ticketFile.DisplayTickets();
     }
     // write data
     else if (choice == "2")
     {
-        StreamReader sr = new StreamReader(file);
-        int lines = 1; //generates TicketID
-
-        while (!sr.EndOfStream)
-        {
-            string line = sr.ReadLine();
-            lines++;
-        }
-
-        sr.Close();
+        Ticket ticket = new Ticket();
+        
         // Get Summary
-        Console.WriteLine("Enter Summary for TicketID #{0}: ", lines);
-        string summary = Console.ReadLine();
+        Console.Write($"Enter Ticket Summary for #{ticket.TicketID}: ");
+        ticket.Summary = Console.ReadLine();
         // Get Status
-        Console.WriteLine("Enter Status for TicketID #{0}: ", lines);
+        Console.WriteLine("Enter Ticket Status for TicketID #{0}: ");
         string status = Console.ReadLine();
         // Get Priority
-        Console.WriteLine("Enter Priority for TicketID #{0}: ", lines);
+        Console.WriteLine("Enter Priority for TicketID #{0}: ");
         string priority = Console.ReadLine();
         // Submitter
-        Console.WriteLine("Enter Submitter for TicketID #{0}: ", lines);
+        Console.WriteLine("Enter Submitter for TicketID #{0}: ");
         string submitter = Console.ReadLine();
         //Assigned
-        Console.WriteLine("Enter Assigned for TicketID #{0}: ", lines);
+        Console.WriteLine("Enter Assigned for TicketID #{0}: ");
         string assigned = Console.ReadLine();
         //  Watching
-        Console.WriteLine("Enter Watching for TicketID #{0}: ", lines);
+        Console.WriteLine("Enter Watching for TicketID #{0}: ");
         string watching = Console.ReadLine();
 
-        StreamWriter sw = new StreamWriter(file, append: true);
-        sw.WriteLine("{0},{1},{2},{3},{4},{5},{6}", lines, summary, status, priority, submitter, assigned, watching);
-        sw.Close();
+        // StreamWriter sw = new StreamWriter(file, append: true);
+        // sw.WriteLine("{0},{1},{2},{3},{4},{5},{6}", lines, summary, status, priority, submitter, assigned, watching);
+        // sw.Close();
     }
 } while (choice == "1" || choice == "2");
 
