5425f15456bb26aa3b024ed97db2181cd21c436b
jmartinezcode <jmartinez.data@gmail.com>
2023-10-20 16:31:52 -0500

    Modified TicketFile - Created new LoadTickets method to handle logic within constructor - Modified ReadTicket method to handle each file and ticket type - Modified AddTicket to handle multiple files and ticket types - Created GetUniqueTicketData to display additional ticket information -Created requisite .csv files with appropriate headers

--- TicketFile.cs
+++ TicketFile.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Microsoft.AspNetCore.Mvc;
 using NLog;
 
@@ -7,61 +8,151 @@ public class TicketFile
     public List<Ticket> Tickets { get; set;  }
     private static NLog.Logger logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
-    public TicketFile(string ticketFilePath)
+    public TicketFile()
     {
-        filePath = ticketFilePath;
         Tickets = new List<Ticket>();
-
-        try
+        // Moved logic to separate method
+        LoadTickets();        
+    }
+    private void LoadTickets()
+    {
+        string[] ticketTypes = { "Bug", "Enhancement", "Task" };
+        foreach (string ticketType in ticketTypes)
         {
-            StreamReader sr = new StreamReader(filePath);
-            sr.ReadLine(); //Skip headers
-            while (!sr.EndOfStream)
+            string filePath;
+            // get proper file
+            switch (ticketType)
             {
-                string line = sr.ReadLine();
-                // create a method to read Ticket 
-                Ticket ticket = ReadTicket(line);
-                Tickets.Add(ticket);
+                case "Bug":
+                    filePath = "Tickets.csv";
+                    break;
+                case "Enhancement":
+                    filePath = $"{ticketType}s.csv";
+                    break;
+                case "Task":
+                    filePath = $"{ticketType}.csv";
+                    break;
+                default:
+                    logger.Error($"Invalid ticket type: {ticketType}");
+                    continue;
             }
-            sr.Close();
-            logger.Info($"Tickets in file: {Tickets.Count}");
 
-        }
-        catch (Exception ex)
-        {
-            logger.Error(ex.Message);
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(filePath);
+                    sr.ReadLine(); //Skip headers
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        // create a method to read Ticket 
+                        Ticket ticket = ReadTicket(line, ticketType); //update new ticketType
+                        Tickets.Add(ticket);
+                    }
+                    sr.Close();
+                    logger.Info($"Tickets in file: {Tickets.Count}");
+
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.Message);
+                }
+            }
         }
     }
 
-    private Ticket ReadTicket(string line)
+    private Ticket ReadTicket(string line, string ticketType)
     {
+        // Modified for different ticket types
         string[] ticketDetails = line.Split(",");
-        Ticket ticket = new Ticket
+        switch (ticketType)
         {
-            TicketID = int.Parse(ticketDetails[0]),
-            Summary = ticketDetails[1],
-            Status = ticketDetails[2],
-            Priority = ticketDetails[3],
-            Submitter = ticketDetails[4],
-            Assigned = ticketDetails[5],
-            Watching = ticketDetails[6].Split("|").ToList()
-        };
-        return ticket;
+            case "Bug":
+                BugTicket bug = new BugTicket
+                {
+                    TicketID = int.Parse(ticketDetails[0]),
+                    Summary = ticketDetails[1],
+                    Status = ticketDetails[2],
+                    Priority = ticketDetails[3],
+                    Submitter = ticketDetails[4],
+                    Assigned = ticketDetails[5],
+                    Watching = ticketDetails[6].Split("|").ToList(),
+                    Severity = ticketDetails[7]
+                };
+                return bug;
+            case "Enhancement":
+                EnhancementTicket enhancement = new EnhancementTicket
+                {
+                    TicketID = int.Parse(ticketDetails[0]),
+                    Summary = ticketDetails[1],
+                    Status = ticketDetails[2],
+                    Priority = ticketDetails[3],
+                    Submitter = ticketDetails[4],
+                    Assigned = ticketDetails[5],
+                    Watching = ticketDetails[6].Split("|").ToList(),
+                    Software = ticketDetails[7],
+                    Cost = decimal.Parse(ticketDetails[8]),
+                    Reason = ticketDetails[9],
+                    Estimate = ticketDetails[10]
+                };
+                return enhancement;
+            case "Task":
+                TaskTicket task = new TaskTicket
+                {
+                    TicketID = int.Parse(ticketDetails[0]),
+                    Summary = ticketDetails[1],
+                    Status = ticketDetails[2],
+                    Priority = ticketDetails[3],
+                    Submitter = ticketDetails[4],
+                    Assigned = ticketDetails[5],
+                    Watching = ticketDetails[6].Split("|").ToList(),
+                    ProjectName = ticketDetails[7],
+                    DueDate = DateTime.Parse(ticketDetails[8])
+                };
+                return task;
+            default:
+                logger.Error($"Failed to read {ticketType} ticket");
+                return null;  
+        }        
     }
     public void AddTicket(Ticket ticket)
     {
         try
         {
+            // Get ticket Type so get proper file
+            string ticketType = ticket.GetTicketType();
+            string filePath;
+            // get proper file
+            switch (ticketType)
+            {
+                case "Bug":
+                    filePath = "Tickets.csv";
+                    break;
+                case "Enhancement":
+                    filePath = $"{ticketType}s.csv";
+                    break;
+                case "Task":
+                    filePath = $"{ticketType}.csv";
+                    break;
+                default:
+                    logger.Error($"Invalid ticket type: {ticketType}");
+                    return;
+            }
             // Generate next ticket ID
             int nextTicketID = Tickets.Max(t => t.TicketID) + 1;
             ticket.TicketID = nextTicketID;
-            StreamWriter sw = new StreamWriter(filePath, true);
-            sw.WriteLine($"{ticket.TicketID},{ticket.Summary},{ticket.Status},{ticket.Priority},{ticket.Submitter},{ticket.Assigned},{string.Join("|", ticket.Watching)}");
-            sw.Close();
+            
+            // change to using so don't need close
+            using (var sw = new StreamWriter(filePath, true))
+            {
+                // write to appropriate csv
+                sw.WriteLine($"{ticket.TicketID},{ticket.Summary},{ticket.Status},{ticket.Priority},{ticket.Submitter},{ticket.Assigned},{string.Join("|", ticket.Watching)},{GetUniqueTicketData(ticket)}");
+            }                        
 
             Tickets.Add(ticket);
-
-            logger.Info("Ticket ID {ID} added", ticket.TicketID);
+            
+            logger.Info($"{ticketType} ticket added. Ticket ID: {ticket.TicketID}");
 
         }
         catch (Exception ex)
@@ -69,11 +160,23 @@ public class TicketFile
             logger.Error(ex.Message);
         }
     }
+    private string GetUniqueTicketData(Ticket ticket)
+    {
+        // Adds unique ticket data to default information
+        if (ticket is BugTicket bug)
+            return $"{bug.Severity}";
+        else if (ticket is EnhancementTicket enhancement)
+            return $"{enhancement.Software},{enhancement.Cost},{enhancement.Reason},{enhancement.Estimate}";
+        else if (ticket is TaskTicket task)
+            return $"{task.ProjectName},{task.DueDate:MM/dd/yy}";
+        else
+            return string.Empty;
+    }
     public void DisplayTickets()
     {
         foreach (var ticket in Tickets)
         {
-            Console.WriteLine(ticket.ToString());
+            Console.WriteLine(ticket.Display());
         }
     }
 }
