a7135c23158af5d5485eb09fc133ead6b279aded
jmartinezcode <jmartinez.data@gmail.com>
2023-10-20 11:27:22 -0500

    Abstract Ticket class - Added support for different ticket types - Changed ToString to display method - Added GetTicketType method

--- Ticket.cs
+++ Ticket.cs
@@ -1,4 +1,4 @@
-public class Ticket
+public abstract class Ticket
 {
     public int TicketID { get; set; }
     public string Summary { get; set;  }
@@ -12,9 +12,52 @@ public class Ticket
     {
         Watching = new List<string>();
     }
-    public override string ToString()
+    public virtual string Display()
     {
         return $"TicketID: {TicketID}\nSummary: {Summary}\nStatus: {Status}\nPriority: {Priority}\nSubmitter: {Submitter}\nAssigned: {Assigned}\nWatching: {string.Join(", ", Watching)}\n";
     }
+    public abstract string GetTicketType();
+}
 
+public class BugTicket : Ticket
+{
+    public string Severity { get; set; }
+    public override string Display()
+    {
+        return base.Display() + $"Severity: {Severity}\n";
+    }
+    public override string GetTicketType()
+    {
+        return "Bug";
+    }
+}
+
+public class EnhancementTicket : Ticket
+{
+    public string Software { get; set; }
+    public decimal Cost { get; set; }
+    public string Reason { get; set; }
+    public string Estimate { get; set; }
+    public override string Display()
+    {
+        return base.Display() + $"Software: {Software}\nCost: {Cost}\nReason: {Reason}\nEstimate: {Estimate}\n";
+    }
+    public override string GetTicketType()
+    {
+        return "Enhancement";
+    }
+}
+
+public class TaskTicket : Ticket
+{
+    public string ProjectName { get; set; }
+    public DateTime DueDate { get; set; }
+    public override string Display()
+    {
+        return base.Display() + $"Project Name: {ProjectName}\nDue Date: {DueDate:MM/DD/YY}";
+    }
+    public override string GetTicketType()
+    {
+        return "Task";
+    }
 }
